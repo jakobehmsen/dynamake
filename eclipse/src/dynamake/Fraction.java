39d43c31e614670cda3d77d5c871bb22ea6650b0
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-05-14 20:26:13 +0200

    For Fraction instances, numerator and denominator was swapped from ints to BigIntegers to prevent the aforementiond issue as to scaling with over- and underflow of ints.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/Fraction.java
+++ eclipse/src/dynamake/Fraction.java
@@ -1,26 +1,40 @@
 package dynamake;
 
+import java.math.BigInteger;
+
 public class Fraction extends Number {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	// Must be replaced by BigInteger to avoid over- and underflow of integers for which cases has already started to occur
-	private int numerator;
-	private int denominator;
+//	private int numerator;
+//	private int denominator;
+	
+	private BigInteger numerator;
+	private BigInteger denominator;
 
 	public Fraction(int value) {
 		this(value, 1);
 	}
 
 	public Fraction(int numerator, int denominator) {
+//		this.numerator = numerator;
+//		this.denominator = denominator;
+		
+		this.numerator = BigInteger.valueOf(numerator);
+		this.denominator = BigInteger.valueOf(denominator);
+	}
+
+	public Fraction(BigInteger numerator, BigInteger denominator) {
 		this.numerator = numerator;
 		this.denominator = denominator;
 	}
 
 	@Override
 	public double doubleValue() {
-		return (double)numerator / denominator;
+//		return numerator / denominator;
+		
+		return numerator.doubleValue() / denominator.doubleValue();
 	}
 
 	@Override
@@ -39,36 +53,54 @@ public class Fraction extends Number {
 	}
 	
 	public Fraction add(Fraction other) {
+//		Fraction f = new Fraction(
+//			numerator * other.denominator + denominator * other.numerator,
+//			denominator * other.denominator
+//		);
+		
 		Fraction f = new Fraction(
-			numerator * other.denominator + denominator * other.numerator,
-			denominator * other.denominator
+			numerator.multiply(other.denominator).add(denominator.multiply(other.numerator)),
+			denominator.multiply(other.denominator)
 		);
 		f.reduce();
 		return f;
 	}
 	
 	public Fraction subtract(Fraction other) {
+//		Fraction f = new Fraction(
+//			numerator * other.denominator - denominator * other.numerator,
+//			denominator * other.denominator
+//		);
+		
 		Fraction f = new Fraction(
-			numerator * other.denominator - denominator * other.numerator,
-			denominator * other.denominator
+			numerator.multiply(other.denominator).subtract(denominator.multiply(other.numerator)),
+			denominator.multiply(other.denominator)
 		);
 		f.reduce();
 		return f;
 	}
 	
 	public Fraction multiply(Fraction other) {
+//		Fraction f = new Fraction(
+//			numerator * other.numerator,
+//			denominator * other.denominator
+//		);
 		Fraction f = new Fraction(
-			numerator * other.numerator,
-			denominator * other.denominator
+			numerator.multiply(other.numerator),
+			denominator.multiply(other.denominator)
 		);
 		f.reduce();
 		return f;
 	}
 	
 	public Fraction divide(Fraction other) {
+//		Fraction f = new Fraction(
+//			numerator * other.denominator,
+//			denominator * other.numerator
+//		);
 		Fraction f = new Fraction(
-			numerator * other.denominator,
-			denominator * other.numerator
+			numerator.multiply(other.denominator),
+			denominator.multiply(other.numerator)
 		);
 		f.reduce();
 		return f;
@@ -87,10 +119,16 @@ public class Fraction extends Number {
 	}
 	
 	private void reduce() {
-		int cd = gcd(numerator, denominator);
-		if(cd > 0) {
-			numerator = numerator / cd;
-			denominator = denominator / cd;
+//		int cd = gcd(numerator, denominator);
+//		if(cd > 0) {
+//			numerator = numerator / cd;
+//			denominator = denominator / cd;
+//		}
+		
+		BigInteger cd = numerator.gcd(denominator);
+		if(!cd.equals(BigInteger.ZERO)) {
+			numerator = numerator.divide(cd);
+			denominator = denominator.divide(cd);
 		}
 	}
 
