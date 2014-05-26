a9014a5cde0641b383cf127784b3c6febf004059
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-05-26 10:55:28 +0200

    Replaced usages of executeOnRoot with PrevayerServiceConnection in creation model.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/CreationModel.java
+++ eclipse/src/dynamake/CreationModel.java
@@ -358,7 +358,7 @@ public class CreationModel extends Model {
 					if(((CreationModel)sender).allArgumentsAreSet()) {
 						final ModelComponent parent = ModelComponent.Util.getParent(view);
 						if(parent != null && parent.getModelBehind() instanceof CanvasModel) {
-							transactionFactory.executeOnRoot(propCtx, new DualCommandFactory<Model>() {
+							connection.execute(propCtx, new DualCommandFactory<Model>() {
 								@Override
 								public void createDualCommands(List<DualCommand<Model>> dualCommands) {
 									dualCommands.add(new DualCommandPair<Model>(
@@ -367,6 +367,7 @@ public class CreationModel extends Model {
 									));
 								}
 							});
+							connection.commit(new PropogationContext(LiveModel.TAG_CAUSED_BY_COMMIT));
 						}
 					}
 				}
