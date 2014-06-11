ab9cf6187d5b8d143b15b41e078d158927b3dcab
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-06-11 14:37:01 +0200

    Increased usage of repaint sync framework in plot tool.

--- eclipse/src/dynamake/PlotTool.java
+++ eclipse/src/dynamake/PlotTool.java
@@ -152,20 +152,14 @@ public class PlotTool implements Tool {
 			
 			factoryPopopMenu.addPopupMenuListener(new PopupMenuListener() {
 				@Override
-				public void popupMenuWillBecomeVisible(PopupMenuEvent e) { 
-					productionPanel.livePanel.repaint();
-				}
+				public void popupMenuWillBecomeVisible(PopupMenuEvent e) { }
 				
 				@Override
-				public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
-//					if(mouseDown != null) {
-//						productionPanel.livePanel.repaint();
-//					}
-				}
+				public void popupMenuWillBecomeInvisible(PopupMenuEvent e) { }
 				
 				@Override
 				public void popupMenuCanceled(PopupMenuEvent e) {
-					productionPanel.editPanelMouseAdapter.clearEffectFrameDirect();
+					productionPanel.editPanelMouseAdapter.clearEffectFrameOnBranch(branchStep2);
 					branch.reject();
 				}
 			});
