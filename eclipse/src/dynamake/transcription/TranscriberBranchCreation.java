eb75a9261f4afe9392a1eb4b4451ededfd7c6fd7
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-06-30 15:17:33 +0200

    Removed redundant transcription related interfaces.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/TranscriberBranchCreation.java
+++
@@ -1,7 +0,0 @@
-package dynamake.transcription;
-
-import dynamake.commands.DualCommand;
-
-public interface TranscriberBranchCreation<T> {
-	void create(DualCommand<T> transaction, TranscriberBranchContinuation<T> continuation);
-}
