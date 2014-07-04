afeead36703a33174e755d00ef804ab34ed97853
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-04 10:28:37 +0200

    Added the following: When changing the location during resizing in the edit tool, and the x,y coordinates changes, each contained model should be offset correspondingly so that it appears as if the canvas is simply stretched out without affecting the position of the contained models.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/tools/EditTool.java
+++ eclipse/src/dynamake/tools/EditTool.java
@@ -1,10 +1,14 @@
 package dynamake.tools;
 
+import java.awt.Component;
 import java.awt.Rectangle;
 import java.util.List;
 
+import javax.swing.JComponent;
+
 import dynamake.commands.DualCommand;
 import dynamake.commands.DualCommandPair;
+import dynamake.models.CanvasModel;
 import dynamake.models.Model;
 import dynamake.models.ModelComponent;
 import dynamake.models.ModelTranscriber;
@@ -21,25 +25,63 @@ public class EditTool extends BoundsChangeTool {
 			List<DualCommand<Model>> dualCommands, ModelComponent selection, Rectangle newBounds) {
 		ModelTranscriber selectionModelTranscriber = selection.getModelTranscriber();
 		Model selectionModel = selection.getModelBehind();
+		Rectangle currentBounds = ((JComponent)selection).getBounds();
 		
-		dualCommands.add(new DualCommandPair<Model>(
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "X", new Fraction(newBounds.x)), 
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "X", selectionModel.getProperty("X"))
-		));
-		
-		dualCommands.add(new DualCommandPair<Model>(
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Y", new Fraction(newBounds.y)), 
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Y", selectionModel.getProperty("Y"))
-		));
-		
-		dualCommands.add(new DualCommandPair<Model>(
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Width", new Fraction(newBounds.width)), 
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Width", selectionModel.getProperty("Width"))
-		));
-		
-		dualCommands.add(new DualCommandPair<Model>(
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Height", new Fraction(newBounds.height)), 
-			new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Height", selectionModel.getProperty("Height"))
-		));
+		if(currentBounds.x != newBounds.x) {
+			dualCommands.add(new DualCommandPair<Model>(
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "X", new Fraction(newBounds.x)), 
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "X", selectionModel.getProperty("X"))
+			));
+			
+			if(currentBounds.width != newBounds.width) {
+				if(selectionModel instanceof CanvasModel) {
+					for(Component childComponent: ((JComponent)selection).getComponents()) {
+						ModelComponent child = (ModelComponent)childComponent;
+						int offset = currentBounds.x - newBounds.x;
+						Number newX = ((Fraction)child.getModelBehind().getProperty("X")).add(new Fraction(offset));
+						dualCommands.add(new DualCommandPair<Model>(
+							new Model.SetPropertyTransaction(child.getModelTranscriber().getModelLocation(), "X", newX), 
+							new Model.SetPropertyTransaction(child.getModelTranscriber().getModelLocation(), "X", selectionModel.getProperty("X"))
+						));
+					}
+				}
+			}
+		}
+
+		if(currentBounds.y != newBounds.y) {
+			dualCommands.add(new DualCommandPair<Model>(
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Y", new Fraction(newBounds.y)), 
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Y", selectionModel.getProperty("Y"))
+			));
+
+			
+			if(currentBounds.height != newBounds.height) {
+				if(selectionModel instanceof CanvasModel) {
+					for(Component childComponent: ((JComponent)selection).getComponents()) {
+						ModelComponent child = (ModelComponent)childComponent;
+						int offset = currentBounds.y - newBounds.y;
+						Number newY = ((Fraction)child.getModelBehind().getProperty("Y")).add(new Fraction(offset));
+						dualCommands.add(new DualCommandPair<Model>(
+							new Model.SetPropertyTransaction(child.getModelTranscriber().getModelLocation(), "Y", newY), 
+							new Model.SetPropertyTransaction(child.getModelTranscriber().getModelLocation(), "Y", selectionModel.getProperty("Y"))
+						));
+					}
+				}
+			}
+		}
+
+		if(currentBounds.width != newBounds.width) {
+			dualCommands.add(new DualCommandPair<Model>(
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Width", new Fraction(newBounds.width)), 
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Width", selectionModel.getProperty("Width"))
+			));
+		}
+
+		if(currentBounds.height != newBounds.height) {
+			dualCommands.add(new DualCommandPair<Model>(
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Height", new Fraction(newBounds.height)), 
+				new Model.SetPropertyTransaction(selectionModelTranscriber.getModelLocation(), "Height", selectionModel.getProperty("Height"))
+			));
+		}
 	}
 }
