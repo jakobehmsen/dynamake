62d60541e7dd7358b5d134fdf815d26332bbad32
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-08 13:33:19 +0200

    Added to DualCommand about potential renaming.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/DualCommand.java
+++ eclipse/src/dynamake/commands/DualCommand.java
@@ -7,6 +7,7 @@ import dynamake.models.PropogationContext;
 import dynamake.transcription.TranscriberBranch;
 import dynamake.transcription.TranscriberCollector;
 
+// TODO: Consider renaming this type of command to ReversibleCommand and reflect this on the DualCommand* types.
 public interface DualCommand<T> extends Serializable {
 	public void executeForwardOn(PropogationContext propCtx, T prevalentSystem, Date executionTime, TranscriberBranch<T> branch, TranscriberCollector<T> collector);
 	public void executeBackwardOn(PropogationContext propCtx, T prevalentSystem, Date executionTime, TranscriberBranch<T> branch, TranscriberCollector<T> collector);
