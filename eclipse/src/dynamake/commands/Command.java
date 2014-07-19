a58cde859a555e48eba92b91566c5fc965d822d6
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-19 21:00:16 +0200

    Added documentation to interface Command.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/Command.java
+++ eclipse/src/dynamake/commands/Command.java
@@ -6,6 +6,11 @@ import dynamake.models.Location;
 import dynamake.models.PropogationContext;
 import dynamake.transcription.Collector;
 
+/**
+ * Instances of implementors each is able to execute on objects and are supposed to be kept elsewhere for e.g. undo/do - thus the Serializable attribute.
+ *
+ * @param <T> The type of object that instances of implementers should support execution on.
+ */
 public interface Command<T> extends Serializable {
 	Object executeOn(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location);
 	
