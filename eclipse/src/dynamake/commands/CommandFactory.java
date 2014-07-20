400247c13d21c4f121092a3f7617212c60bcde33
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-20 22:37:29 +0200

    Added documentation for interface CommandFactory.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/CommandFactory.java
+++ eclipse/src/dynamake/commands/CommandFactory.java
@@ -1,7 +1,9 @@
 package dynamake.commands;
 
 import java.io.Serializable;
-
+/**
+ * Instances of implementors are supposed to be able to create commands based on an output made from the execution of a previous command.
+ */
 public interface CommandFactory<T> extends Serializable {
 	Command<T> createCommand(Object output);
 }
