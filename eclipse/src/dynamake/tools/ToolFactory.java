3af6d0492d981a7cb05be70757ea2c136af274a4
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-20 22:32:49 +0200

    Added documentation for interface ToolFactory.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/tools/ToolFactory.java
+++ eclipse/src/dynamake/tools/ToolFactory.java
@@ -1,5 +1,8 @@
 package dynamake.tools;
 
+/**
+ * Instances of implementors are supposed to be able to create tools and describes the kind of tool.
+ */
 public interface ToolFactory {
 	String getName();
 	Tool createTool();
