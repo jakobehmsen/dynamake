1d1d7b755459602d60cce6c6793e3a9e46ae6aca
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-20 22:39:59 +0200

    Added documentation for PendingCommandFactory.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/PendingCommandFactory.java
+++ eclipse/src/dynamake/commands/PendingCommandFactory.java
@@ -2,6 +2,9 @@ package dynamake.commands;
 
 import java.util.List;
 
+/**
+ * Instances of implementors are supposed to be able to commands in a pending state.
+ */
 public interface PendingCommandFactory<T> {
 	T getReference();
 	void createPendingCommand(List<CommandState<T>> commandStates);
