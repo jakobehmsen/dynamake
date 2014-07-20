59ddd7d0097041e063ed7c8f343b9f0ccc19e4bb
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-20 22:12:38 +0200

    Added documentation for interface CommandState.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/CommandState.java
+++ eclipse/src/dynamake/commands/CommandState.java
@@ -6,6 +6,15 @@ import dynamake.models.Location;
 import dynamake.models.PropogationContext;
 import dynamake.transcription.Collector;
 
+/**
+ * Instances of implementors are supposed to represent commands in different states. 
+ * For now, these states include pending, undoable, and redoable.
+ * 
+ * Further, when such instances are executed, a new command state is returned representing the state of a command in state relation context.
+ * For now, such relation context is as follows: pending => (undoable => redoable)*
+ *
+ * @param <T>
+ */
 public interface CommandState<T> extends Serializable {
 	public CommandState<T> executeOn(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location);
 }
