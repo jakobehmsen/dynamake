fc46ff0f98403148740f20529aedc3e009addb4e
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-20 22:44:01 +0200

    Added documentation for interface Collector.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/Collector.java
+++ eclipse/src/dynamake/transcription/Collector.java
@@ -1,5 +1,8 @@
 package dynamake.transcription;
 
+/**
+ * Instances of implementors provided methods to indicate and manage different kinds of side effects.
+ */
 public interface Collector<T> {
 	void execute(Object command);
 	void afterNextTrigger(Runnable runnable);
