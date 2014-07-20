19b33f31892fbd8457e90e9849939261773c67d8
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-20 22:42:38 +0200

    Added documentation for interface Trigger.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/Trigger.java
+++ eclipse/src/dynamake/transcription/Trigger.java
@@ -1,5 +1,8 @@
 package dynamake.transcription;
 
+/**
+ * Instances of implementors represents triggers that indicate side effects on a collector.
+ */
 public interface Trigger<T> {
 	void run(Collector<T> collector);
 }
