dde89dd499e981f86fca1cbc477a93202fb50970
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-21 13:19:30 +0200

    Added model factory NewInstanceFactory to support creation of "instances" of models using "HistoryChangeForwarder"s.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/models/factories/NewInstanceFactory.java
@@ -0,0 +1,36 @@
+package dynamake.models.factories;
+
+import dynamake.models.CompositeLocation;
+import dynamake.models.HistoryChangeForwarder;
+import dynamake.models.Location;
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.transcription.Collector;
+
+public class NewInstanceFactory implements ModelFactory {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Location modelLocation;
+	
+	public NewInstanceFactory(Location modelLocation) {
+		this.modelLocation = modelLocation;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Model create(Model rootModel, PropogationContext propCtx, int propDistance, Collector<Model> collector, Location location) {
+		Model inhereter = (Model)CompositeLocation.getChild(rootModel, location, modelLocation);
+		Model instance = inhereter.cloneDeep();
+		
+		inhereter.addObservee(new HistoryChangeForwarder(instance));
+		
+		return instance;
+	}
+}
