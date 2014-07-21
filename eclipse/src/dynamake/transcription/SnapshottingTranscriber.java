2d1392c039704a0ca8edcf0004e760039db74b3b
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-21 11:28:22 +0200

    Did a bit of cleaning up.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/SnapshottingTranscriber.java
+++ eclipse/src/dynamake/transcription/SnapshottingTranscriber.java
@@ -423,8 +423,6 @@ public class SnapshottingTranscriber<T> implements Transcriber<T> {
 								flushedTransactionsFromReference.addAll(undoables);
 								
 								// Update the log of each affected model isolately; no transaction is cross-model
-//								RevertingCommandStateSequence<T> transactionFromReference = RevertingCommandStateSequence.reverse(undoables);
-								
 								for(CommandState<T> undoable: undoables)
 									((Model)reference).log((CommandState<Model>)undoable);
 							} else
