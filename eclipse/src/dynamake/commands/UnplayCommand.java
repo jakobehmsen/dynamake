5c1c3cb1835d4572684864bf9fdebd2706fc4162
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-24 23:16:51 +0200

    Did a bit of cleaning up.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/UnplayCommand.java
+++ eclipse/src/dynamake/commands/UnplayCommand.java
@@ -1,12 +1,8 @@
 package dynamake.commands;
 
-import java.io.Serializable;
-import java.util.List;
-
 import dynamake.models.Location;
 import dynamake.models.Model;
 import dynamake.models.PropogationContext;
-import dynamake.models.Model.PendingUndoablePair;
 import dynamake.transcription.Collector;
 
 public class UnplayCommand implements Command<Model> {
@@ -14,11 +10,6 @@ public class UnplayCommand implements Command<Model> {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-//	private List<Model.PendingUndoablePair> pendingUndoablePairs;
-//
-//	public UnplayCommand(List<Model.PendingUndoablePair> pendingUndoablePairs) {
-//		this.pendingUndoablePairs = pendingUndoablePairs;
-//	}
 
 	@Override
 	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location) {
