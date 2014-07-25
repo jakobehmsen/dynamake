9cce7cf068e3b7fa3b0046438e0e627813b0c415
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-25 15:25:18 +0200

    Added two new output classes represents output of for undo- and redo commands regarding the specific kind of command.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/RedoCommand.java
+++ eclipse/src/dynamake/commands/RedoCommand.java
@@ -1,5 +1,7 @@
 package dynamake.commands;
 
+import java.io.Serializable;
+
 import dynamake.models.Location;
 import dynamake.models.Model;
 import dynamake.models.PropogationContext;
@@ -7,6 +9,13 @@ import dynamake.transcription.Collector;
 import dynamake.transcription.IsolatingCollector;
 
 public class RedoCommand implements Command<Model> {
+	public static class Output implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+	}
+	
 	/*
 	 * 
 	 */
@@ -26,6 +35,6 @@ public class RedoCommand implements Command<Model> {
 		
 		model.redo(propCtx, 0, collector);
 		
-		return null;
+		return new Output();
 	}
 }
--- eclipse/src/dynamake/commands/UndoCommand.java
+++ eclipse/src/dynamake/commands/UndoCommand.java
@@ -1,5 +1,7 @@
 package dynamake.commands;
 
+import java.io.Serializable;
+
 import dynamake.models.Location;
 import dynamake.models.Model;
 import dynamake.models.PropogationContext;
@@ -7,6 +9,13 @@ import dynamake.transcription.Collector;
 import dynamake.transcription.IsolatingCollector;
 
 public class UndoCommand implements Command<Model> {
+	public static class Output implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+	}
+	
 	/**
 	 * 
 	 */
@@ -26,6 +35,6 @@ public class UndoCommand implements Command<Model> {
 		
 		model.undo(propCtx, 0, collector);
 		
-		return null;
+		return new Output();
 	}
 }
