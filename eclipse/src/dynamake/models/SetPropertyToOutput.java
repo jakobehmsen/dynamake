6ce1712b9880036fc7379674626cbd8960c79262
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-26 21:33:18 +0200

    Added command SetPropertyToOutput to support recording command outputs on models.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/models/SetPropertyToOutput.java
@@ -0,0 +1,28 @@
+package dynamake.models;
+
+import dynamake.commands.Command;
+import dynamake.transcription.Collector;
+
+public class SetPropertyToOutput implements Command<Model> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private Command<Model> command;
+
+	public SetPropertyToOutput(String name, Command<Model> command) {
+		this.name = name;
+		this.command = command;
+	}
+
+	@Override
+	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location) {
+		Model model = (Model)location.getChild(prevalentSystem);
+		
+		Object output = command.executeOn(propCtx, prevalentSystem, collector, location);
+		model.setProperty(name, output, propCtx, 0, collector);
+		
+		return output;
+	}
+}
