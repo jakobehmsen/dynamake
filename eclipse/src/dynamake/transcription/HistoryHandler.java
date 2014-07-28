e8b05cc7d644bfaefddef5bb057c785af1f32f8a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-28 11:33:43 +0200

    Added interface HistoryHandler to decouple handling of executed command versus their references.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/transcription/HistoryHandler.java
@@ -0,0 +1,12 @@
+package dynamake.transcription;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.models.Model.PendingUndoablePair;
+
+public interface HistoryHandler<T> extends Serializable {
+	void logFor(T reference, ArrayList<PendingUndoablePair> pendingUndoablePairs, PropogationContext propCtx, int propDistance, Collector<Model> collector);
+}
