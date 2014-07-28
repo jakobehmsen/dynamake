a8961d2a26bf822a3c70608f97cfa395bd9f047a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-28 12:24:13 +0200

    Added methods startLog and rejectLog in history handler to be used at the respective events in connection.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/HistoryHandler.java
+++ eclipse/src/dynamake/transcription/HistoryHandler.java
@@ -7,5 +7,8 @@ import dynamake.models.PropogationContext;
 import dynamake.models.Model.PendingUndoablePair;
 
 public interface HistoryHandler<T> extends Serializable {
+	void startLogFor(T reference, PropogationContext propCtx, int propDistance, Collector<T> collector);
 	void logFor(T reference, ArrayList<PendingUndoablePair> pendingUndoablePairs, PropogationContext propCtx, int propDistance, Collector<T> collector);
+	void commitLogFor(T reference, PropogationContext propCtx, int propDistance, Collector<T> collector);
+	void rejectLogFor(T reference, PropogationContext propCtx, int propDistance, Collector<T> collector);
 }
