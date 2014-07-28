419aa8d697e38ae03a02109dc343cca266111a8e
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-07-28 12:00:16 +0200

    Added collection type Categorizer to categorize collections of items.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/collections/Categorizer.java
@@ -0,0 +1,32 @@
+package dynamake.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.List;
+
+public class Categorizer<T, U> {
+	private Hashtable<T, List<U>> categories = new Hashtable<T, List<U>>();
+	
+	public void add(T category, U item) {
+		List<U> itemsInCategory = categories.get(category);
+		if(itemsInCategory == null) {
+			itemsInCategory = new ArrayList<U>();
+			categories.put(category, itemsInCategory);
+		}
+		itemsInCategory.add(item);
+	}
+	
+	public void remove(T category, U item) {
+		List<U> itemsInCategory = categories.get(category);
+		if(itemsInCategory != null)
+			itemsInCategory.remove(category);
+	}
+	
+	public List<U> getItems(T category) {
+		List<U> itemsInCategory = categories.get(category);
+		if(itemsInCategory != null)
+			return new ArrayList<U>(itemsInCategory);
+		return Collections.emptyList();
+	}
+}
