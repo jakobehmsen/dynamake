5c8f09dc72f8b0aaba636e15c6aea50d273b5286
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-01 16:58:39 +0200

    Improved support for immediate instance creation of complext structures for a new instance.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/PushLocalChangesCommand.java
+++ eclipse/src/dynamake/commands/PushLocalChangesCommand.java
@@ -37,13 +37,26 @@ public class PushLocalChangesCommand implements MappableCommand<Model> {
 	private List<CommandState<Model>> playThenReverseChanges(Model source, Model target, PropogationContext propCtx, int propDistance, Collector<Model> collector) {
 		List<CommandState<Model>> reversedForwardedChanges = target.playThenReverse(source.getLocalChanges(), propCtx, propDistance, collector);
 		
-		for(CommandState<Model> changeToForward: source.getLocalChangesWithOutput()) {
-			Model.UndoRedoPart urpChangeToForward = (Model.UndoRedoPart)changeToForward;
-			PendingCommandState<Model> pcsChangeToForward = (PendingCommandState<Model>)urpChangeToForward.origin;
-			if(pcsChangeToForward.getCommand() instanceof CanvasModel.AddModelCommand) {
-				CanvasModel.AddModelCommand.Output addModelOutput = (CanvasModel.AddModelCommand.Output)((ReversibleCommand<Model>)urpChangeToForward.revertible).getOutput();
-				Model embeddedSource = ((CanvasModel)source).getModelByLocation(addModelOutput.location);
-				Model embeddedTarget = ((CanvasModel)target).getModelByLocation(addModelOutput.location);
+//		for(CommandState<Model> changeToForward: source.getLocalChangesWithOutput()) {
+//			Model.UndoRedoPart urpChangeToForward = (Model.UndoRedoPart)changeToForward;
+//			PendingCommandState<Model> pcsChangeToForward = (PendingCommandState<Model>)urpChangeToForward.origin;
+//			if(pcsChangeToForward.getCommand() instanceof CanvasModel.AddModelCommand) {
+//				CanvasModel.AddModelCommand.Output addModelOutput = (CanvasModel.AddModelCommand.Output)((ReversibleCommand<Model>)urpChangeToForward.revertible).getOutput();
+//				Model embeddedSource = ((CanvasModel)source).getModelByLocation(addModelOutput.location);
+//				Model embeddedTarget = ((CanvasModel)target).getModelByLocation(addModelOutput.location);
+//				
+//				List<CommandState<Model>> reversedEmbeddedForwardedChanges = playThenReverseChanges(embeddedSource, embeddedTarget, propCtx, propDistance, collector);
+//				reversedForwardedChanges.addAll(reversedEmbeddedForwardedChanges);
+//			}
+//		}
+		
+		if(source instanceof CanvasModel) {
+			CanvasModel sourceCanvas = (CanvasModel)source;
+			CanvasModel targetCanvas = (CanvasModel)target;
+			
+			for(Location modelLocation: sourceCanvas.getLocations()) {
+				Model embeddedSource = sourceCanvas.getModelByLocation(modelLocation);
+				Model embeddedTarget = targetCanvas.getModelByLocation(modelLocation);
 				
 				List<CommandState<Model>> reversedEmbeddedForwardedChanges = playThenReverseChanges(embeddedSource, embeddedTarget, propCtx, propDistance, collector);
 				reversedForwardedChanges.addAll(reversedEmbeddedForwardedChanges);
