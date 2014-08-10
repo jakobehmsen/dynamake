12b5289cb353b576640e05e6f4173977db258155
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-10 11:48:00 +0200

    Improved PushForwardFromCommand by recursively pushing forwarding creation and local changes throughout the content of the source.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/PushForwardFromCommand.java
+++ eclipse/src/dynamake/commands/PushForwardFromCommand.java
@@ -3,12 +3,14 @@ package dynamake.commands;
 import java.util.ArrayList;
 import java.util.List;
 
+import dynamake.models.CanvasModel;
 import dynamake.models.Location;
 import dynamake.models.Model;
 import dynamake.models.PropogationContext;
 import dynamake.models.RestorableModel;
 import dynamake.transcription.Collector;
 import dynamake.transcription.SimpleExPendingCommandFactory2;
+import dynamake.transcription.Trigger;
 
 public class PushForwardFromCommand implements ForwardableCommand<Model> {
 	/**
@@ -34,6 +36,35 @@ public class PushForwardFromCommand implements ForwardableCommand<Model> {
 		Model target = (Model)location.getChild(prevalentSystem);
 		Model source = (Model)locationOfSourceFromTarget.getChild(target);
 		
+		pushForward(source, target, collector);
+		
+//		ArrayList<CommandState<Model>> toForward = new ArrayList<CommandState<Model>>();
+//		
+//		@SuppressWarnings("unchecked")
+//		List<CommandState<Model>> sourceCreation = (List<CommandState<Model>>)source.getProperty(RestorableModel.PROPERTY_CREATION);
+//		
+//		if(sourceCreation != null) {
+//			toForward.addAll(sourceCreation);
+//		}
+//		
+//		toForward.addAll(source.getLocalChanges());
+//		
+//		if(toForward.size() > 0) {
+//			ArrayList<CommandState<Model>> forwardedCreation = new ArrayList<CommandState<Model>>();
+//			for(CommandState<Model> creationPart: toForward) {
+//				creationPart = creationPart.mapToReferenceLocation(source, target);
+//				for(int i = 0; i < forwardCount; i++)
+//					creationPart = creationPart.forForwarding();
+//				creationPart.appendPendings(forwardedCreation);
+//			}
+//			
+//			collector.execute(new SimpleExPendingCommandFactory2<Model>(target, forwardedCreation));
+//		}
+
+		return null;
+	}
+	
+	private void pushForward(Model source, Model target, Collector<Model> collector) {		
 		ArrayList<CommandState<Model>> toForward = new ArrayList<CommandState<Model>>();
 		
 		@SuppressWarnings("unchecked")
@@ -56,8 +87,25 @@ public class PushForwardFromCommand implements ForwardableCommand<Model> {
 			
 			collector.execute(new SimpleExPendingCommandFactory2<Model>(target, forwardedCreation));
 		}
+		
+		if(source instanceof CanvasModel) {
+			final CanvasModel sourceCanvas = (CanvasModel)source;
+			final CanvasModel targetCanvas = (CanvasModel)target;
 
-		return null;
+			for(final Location locationInSource: sourceCanvas.getLocations()) {
+				final Location locationInTarget = locationInSource.forForwarding();
+				
+				collector.execute(new Trigger<Model>() {
+					@Override
+					public void run(Collector<Model> collector) {
+						Model modelInSource = sourceCanvas.getModelByLocation(locationInSource);
+						Model modelInTarget = targetCanvas.getModelByLocation(locationInTarget);
+						
+						pushForward(modelInSource, modelInTarget, collector);
+					}
+				});
+			}
+		}
 	}
 	
 	@Override
