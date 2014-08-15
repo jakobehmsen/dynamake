da5c13b95c8337574d545acc8ac4d52b377983ff
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-15 22:45:30 +0200

    Added usage of trigger in redo tool, in which the check of canRedo is performed, such that it is ensured to a single command created within the pending command factory.

--- eclipse/src/dynamake/tools/RedoTool.java
+++ eclipse/src/dynamake/tools/RedoTool.java
@@ -14,31 +14,38 @@ import dynamake.transcription.Collector;
 import dynamake.transcription.Connection;
 import dynamake.transcription.ExPendingCommandFactory;
 import dynamake.transcription.TranscribeOnlyPendingCommandFactory;
+import dynamake.transcription.Trigger;
 
 public class RedoTool implements Tool {
 	@Override
 	public void mouseReleased(ProductionPanel productionPanel, MouseEvent e, final ModelComponent modelOver, Connection<Model> connection, Collector<Model> collector) {
-		collector.execute(
-			ExPendingCommandFactory.Util.sequence(new TranscribeOnlyPendingCommandFactory<Model> () {
-				@Override
-				public Model getReference() {
-					return modelOver.getModelBehind();
+		collector.execute(new Trigger<Model>() {
+			@Override
+			public void run(Collector<Model> collector) {
+				if(modelOver.getModelBehind().canRedo()) {
+					collector.execute(
+						ExPendingCommandFactory.Util.sequence(new TranscribeOnlyPendingCommandFactory<Model> () {
+							@Override
+							public Model getReference() {
+								return modelOver.getModelBehind();
+							}
+							
+							@Override
+							public void createPendingCommands(List<CommandState<Model>> commandStates) {
+								commandStates.add(
+									new PendingCommandState<Model>(
+										new RedoCommand(false),
+										new UndoCommand(false)
+									)
+								);
+							}
+						})
+					);
+					
+					collector.commit();
 				}
-				
-				@Override
-				public void createPendingCommands(List<CommandState<Model>> commandStates) {
-					if(modelOver.getModelBehind().canRedo()) {
-						commandStates.add(
-							new PendingCommandState<Model>(
-								new RedoCommand(false),
-								new UndoCommand(false)
-							)
-						);
-					}
-				}
-			})
-		);
-		collector.commit();
+			}
+		});
 	}
 
 	@Override
