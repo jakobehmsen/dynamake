370f2597c1d250e0db6980ddcd93762816d27bef
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-15 22:57:01 +0200

    Removed redundant inner class Util of ExPendingCommandFactory.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/ExPendingCommandFactory.java
+++ eclipse/src/dynamake/transcription/ExPendingCommandFactory.java
@@ -3,7 +3,6 @@ package dynamake.transcription;
 import java.util.List;
 
 import dynamake.commands.CommandState;
-import dynamake.commands.PendingCommandFactory;
 import dynamake.models.PropogationContext;
 
 public interface ExPendingCommandFactory<T> {
@@ -11,51 +10,4 @@ public interface ExPendingCommandFactory<T> {
 	void createPendingCommands(List<CommandState<T>> pendingCommands);
 	void afterPropogationFinished(List<Execution<T>> pendingUndoablePairs, PropogationContext propCtx, int propDistance, Collector<T> collector);
 	Class<? extends HistoryHandler<T>> getHistoryHandlerClass();
-	
-	public static class Util {
-		public static <T> ExPendingCommandFactory<T> sequence(final PendingCommandFactory<T> f) {
-			return new ExPendingCommandFactory<T>() {
-				@Override
-				public T getReference() {
-					return f.getReference();
-				}
-
-				@Override
-				public void createPendingCommands(List<CommandState<T>> pendingCommands) {
-					f.createPendingCommands(pendingCommands);
-				}
-
-				@Override
-				public void afterPropogationFinished(List<Execution<T>> pendingUndoablePairs, PropogationContext propCtx, int propDistance, Collector<T> collector) {
-
-				}
-
-				@SuppressWarnings("unchecked")
-				@Override
-				public Class<? extends HistoryHandler<T>> getHistoryHandlerClass() {
-					if(f instanceof TranscribeOnlyAndPostNotPendingCommandFactory)
-						return (Class<? extends HistoryHandler<T>>)NullHistoryHandler.class;
-					else if(f instanceof TranscribeOnlyPendingCommandFactory)
-						return (Class<? extends HistoryHandler<T>>)PostOnlyHistoryHandler.class;
-					else
-						return (Class<? extends HistoryHandler<T>>)LocalHistoryHandler.class;
-				}
-			};
-		}
-		
-		public static <T> void sequenceEach(Collector<T> collector, final T reference, final List<CommandState<T>> pendingCommandsToSequentialize) {
-			sequenceEach(collector, reference, pendingCommandsToSequentialize, 0);
-		}
-		
-		private static <T> void sequenceEach(Collector<T> collector, final T reference, final List<CommandState<T>> pendingCommandsToSequentialize, final int i) {
-			if(i < pendingCommandsToSequentialize.size()) {
-				collector.execute(new SimpleExPendingCommandFactory<T>(reference, pendingCommandsToSequentialize.get(i)) {
-					@Override
-					public void afterPropogationFinished(List<Execution<T>> pendingUndoablePairs, PropogationContext propCtx, int propDistance, Collector<T> collector) {
-						sequenceEach(collector, reference, pendingCommandsToSequentialize, i + 1);
-					}
-				});
-			}
-		}
-	}
 }
