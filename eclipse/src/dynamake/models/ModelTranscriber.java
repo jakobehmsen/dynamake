904ae96807153364aa66f8dcbec27b8c976b925b
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-17 16:03:18 +0200

    Did some cleaning up in ModelTranscriber.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/models/ModelTranscriber.java
+++ eclipse/src/dynamake/models/ModelTranscriber.java
@@ -65,14 +65,6 @@ public class ModelTranscriber {
 		}
 	}
 
-//	public Locator extendLocator(Locator otherLocator) {
-//		return new CompositeModelLocator(getModelLocator(), otherLocator); 
-//	}
-//
-//	public Location extendLocation(Location otherLocation) {
-//		return new CompositeLocation(getModelLocation(), otherLocation);
-//	}
-
 	public Connection<Model> createConnection() {
 		return transcriber.createConnection(new TriggerHandler<Model>() {
 			@Override
