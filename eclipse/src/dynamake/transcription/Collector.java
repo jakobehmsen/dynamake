76c12a187b99c657a0df8e030ab6ed771d20e1a2
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-22 18:29:56 +0200

    Added comments to collector concerned with future support for shared command scopes during transactions.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/Collector.java
+++ eclipse/src/dynamake/transcription/Collector.java
@@ -4,6 +4,14 @@ package dynamake.transcription;
  * Instances of implementors provided methods to indicate and manage different kinds of side effects.
  */
 public interface Collector<T> {
+	/*
+	Somehow, the collector interface should be extended such that it is possible to indicate the start of a transaction
+	where this start of the transaction must be accompanied by a history handler and a command scope. The history handler
+	is then used for the subsequent logging of command executions - and the scope is supplied for the commands to be executed.
+	Once a transaction is committed, the supplied scope is somehow given to the history handler, when it is requested to
+	commit its collected commands. The scope seems to have a close relation to the history handler - maybe a history handler
+	should be aware of the scope itself and even be able to yield the scope outwards.
+	*/
 	void execute(Object command);
 	void afterNextTrigger(Runnable runnable);
 	void reject();
