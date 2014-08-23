23eccdbcf92d9218aa086c1a80d7f23b2e2840dd
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-23 10:57:15 +0200

    Added comments to method logFor regarding side-effects and importance of the parameter collector.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/transcription/TransactionHandler.java
+++ eclipse/src/dynamake/transcription/TransactionHandler.java
@@ -7,6 +7,7 @@ import dynamake.models.PropogationContext;
 
 public interface TransactionHandler<T> extends Serializable {
 	void startLogFor(T reference, PropogationContext propCtx, int propDistance);
+	// Side-effects are valid here. Thus, collector parameter is needed to support this.
 	void logFor(T reference, ArrayList<Execution<T>> pendingUndoablePairs, PropogationContext propCtx, int propDistance, Collector<T> collector);
 	void commitLogFor(T reference, PropogationContext propCtx, int propDistance);
 	void rejectLogFor(T reference, PropogationContext propCtx, int propDistance);
