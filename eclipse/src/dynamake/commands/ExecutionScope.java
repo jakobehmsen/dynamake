a120b751fa4a7bbfcbc73b78a262c120e5c715d5
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-25 20:14:59 +0200

    Marked ExecutionScope as Serializable.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/ExecutionScope.java
+++ eclipse/src/dynamake/commands/ExecutionScope.java
@@ -1,5 +1,11 @@
 package dynamake.commands;
 
-public class ExecutionScope {
+import java.io.Serializable;
+
+public class ExecutionScope implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
