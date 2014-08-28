4b1e532e7885ff68933e66da3fa05fbb13296d19
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-28 10:29:25 +0200

    Renamed PURCommand to TriStatePURCommand and added a new PURCommand interface, which TriStatePURCommand implements.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/PURCommand.java
+++ eclipse/src/dynamake/commands/PURCommand.java
@@ -1,66 +1,7 @@
 package dynamake.commands;
 
-import dynamake.models.Location;
-import dynamake.models.PropogationContext;
-import dynamake.models.transcription.PostOnlyTransactionHandler;
-import dynamake.transcription.Collector;
-
-// PUR is shorthand for Pending, Undo, Redo
-public class PURCommand<T> implements Command<T> {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	public static final int STATE_PENDING = 0;
-	public static final int STATE_UNDO = 1;
-	public static final int STATE_REDO = 2;
-	
-	private int state;
-	private ReversibleCommand<T> pending;
-	private ReversibleCommand<T> undo;
-	private ReversibleCommand<T> redo;
-
-	public PURCommand(ReversibleCommand<T> pending, ReversibleCommand<T> undo, ReversibleCommand<T> redo) {
-		this(STATE_PENDING, pending, undo, redo);
-	}
-
-	private PURCommand(int state, ReversibleCommand<T> pending, ReversibleCommand<T> undo, ReversibleCommand<T> redo) {
-		this.state = state;
-		this.pending = pending;
-		this.undo = undo;
-		this.redo = redo;
-	}
-
-	@Override
-	public Object executeOn(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location, ExecutionScope scope) {
-		@SuppressWarnings("unchecked")
-		T reference = (T)location.getChild(prevalentSystem);
-		
-		collector.startTransaction(reference, PostOnlyTransactionHandler.class);
-		
-		switch(state) {
-		case STATE_PENDING:
-			collector.execute(pending);
-			break;
-		case STATE_UNDO:
-			collector.execute(undo);
-			break;
-		case STATE_REDO:
-			collector.execute(redo);
-			break;
-		}
-		
-		collector.commitTransaction();
-		
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public PURCommand<T> inUndoState() {
-		return new PURCommand<T>(STATE_UNDO, pending, undo, redo);
-	}
-
-	public PURCommand<T> inRedoState() {
-		return new PURCommand<T>(STATE_REDO, pending, undo, redo);
-	}
+public interface PURCommand<T> extends ReversibleCommand<T> {
+	PURCommand<T> inReplayState();
+	PURCommand<T> inUndoState();
+	PURCommand<T> inRedoState();
 }
---
+++ eclipse/src/dynamake/commands/TriStatePURCommand.java
@@ -0,0 +1,92 @@
+package dynamake.commands;
+
+import dynamake.models.Location;
+import dynamake.models.PropogationContext;
+import dynamake.models.transcription.PostOnlyTransactionHandler;
+import dynamake.transcription.Collector;
+
+// PUR is shorthand for Pending, Undo, Redo
+public class TriStatePURCommand<T> implements PURCommand<T> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final int STATE_PENDING = 0;
+	public static final int STATE_UNDO = 1;
+	public static final int STATE_REDO = 2;
+	
+	private int state;
+	private ReversibleCommand<T> pending;
+	private ReversibleCommand<T> undo;
+	private ReversibleCommand<T> redo;
+
+	public TriStatePURCommand(ReversibleCommand<T> pending, ReversibleCommand<T> undo, ReversibleCommand<T> redo) {
+		this(STATE_PENDING, pending, undo, redo);
+	}
+
+	private TriStatePURCommand(int state, ReversibleCommand<T> pending, ReversibleCommand<T> undo, ReversibleCommand<T> redo) {
+		this.state = state;
+		this.pending = pending;
+		this.undo = undo;
+		this.redo = redo;
+	}
+
+	@Override
+	public void executeForward(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location, ExecutionScope scope) {
+		@SuppressWarnings("unchecked")
+		T reference = (T)location.getChild(prevalentSystem);
+		
+		collector.startTransaction(reference, PostOnlyTransactionHandler.class);
+		
+		switch(state) {
+		case STATE_PENDING:
+			collector.execute(pending);
+			break;
+		case STATE_UNDO:
+			collector.execute(undo);
+			break;
+		case STATE_REDO:
+			collector.execute(redo);
+			break;
+		}
+		
+		collector.commitTransaction();
+	}
+
+	@Override
+	public void executeBackward(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location, ExecutionScope scope) {
+		@SuppressWarnings("unchecked")
+		T reference = (T)location.getChild(prevalentSystem);
+		
+		collector.startTransaction(reference, PostOnlyTransactionHandler.class);
+		
+		switch(state) {
+		case STATE_PENDING:
+			collector.execute(undo);
+			break;
+		case STATE_UNDO:
+			collector.execute(redo);
+			break;
+		case STATE_REDO:
+			collector.execute(undo);
+			break;
+		}
+		
+		collector.commitTransaction();
+	}
+
+	@Override
+	public PURCommand<T> inReplayState() {
+		return new TriStatePURCommand<T>(STATE_PENDING, pending, undo, redo);
+	}
+
+	@Override
+	public PURCommand<T> inUndoState() {
+		return new TriStatePURCommand<T>(STATE_UNDO, pending, undo, redo);
+	}
+
+	@Override
+	public PURCommand<T> inRedoState() {
+		return new TriStatePURCommand<T>(STATE_REDO, pending, undo, redo);
+	}
+}
