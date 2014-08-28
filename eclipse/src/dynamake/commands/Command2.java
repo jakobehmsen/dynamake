7108c2c2bd9867fab140590041842c078b1ccc04
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-28 21:37:39 +0200

    Removed redundant interface Command2.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/Command2.java
+++
@@ -1,9 +0,0 @@
-package dynamake.commands;
-
-import dynamake.models.Location;
-import dynamake.models.PropogationContext;
-import dynamake.transcription.Collector;
-
-public interface Command2<T> {
-	void executeOn(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location, ExecutionScope scope);
-}
