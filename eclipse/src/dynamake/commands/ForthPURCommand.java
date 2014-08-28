200d31ae4d8a3b5f2123c005eac1b45d9bb4a97a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-28 10:31:16 +0200

    Added BackPURCommand and ForthPURCommand to serve as wrappers around ReversibleCommand to behave as PURCommand's.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/commands/ForthPURCommand.java
@@ -0,0 +1,42 @@
+package dynamake.commands;
+
+import dynamake.models.Location;
+import dynamake.models.PropogationContext;
+import dynamake.transcription.Collector;
+
+public class ForthPURCommand<T> implements PURCommand<T> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ReversibleCommand<T> reversibleCommand;
+	
+	public ForthPURCommand(ReversibleCommand<T> reversibleCommand) {
+		this.reversibleCommand = reversibleCommand;
+	}
+
+	@Override
+	public void executeForward(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location, ExecutionScope scope) {
+		reversibleCommand.executeForward(propCtx, prevalentSystem, collector, location, scope);
+	}
+
+	@Override
+	public void executeBackward(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location location, ExecutionScope scope) {
+		reversibleCommand.executeBackward(propCtx, prevalentSystem, collector, location, scope);
+	}
+
+	@Override
+	public PURCommand<T> inReplayState() {
+		return this;
+	}
+
+	@Override
+	public PURCommand<T> inUndoState() {
+		return new BackPURCommand<T>(reversibleCommand);
+	}
+
+	@Override
+	public PURCommand<T> inRedoState() {
+		return this;
+	}
+}
