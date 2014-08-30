f57a03fab5c1304d1dea90013d0b2f0897c339fa
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-30 13:06:58 +0200

    Added add- and remove observer command which uses scope.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/commands/AddObserverCommandFromScope.java
@@ -0,0 +1,28 @@
+package dynamake.commands;
+
+import dynamake.models.CompositeLocation;
+import dynamake.models.Location;
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.transcription.Collector;
+
+public class AddObserverCommandFromScope implements Command<Model> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location, ExecutionScope scope) {
+		Location observableLocation = (Location)scope.consume();
+		Location observerLocation = (Location)scope.consume();
+		
+		Model observable = (Model)new CompositeLocation(location, observableLocation).getChild(prevalentSystem);
+		Model observer = (Model)new CompositeLocation(location, observerLocation).getChild(prevalentSystem);
+		
+		observable.addObserver(observer);
+//		System.out.println(observer + " now observes " + observable);
+		
+		return null;
+	}
+}
---
+++ eclipse/src/dynamake/commands/RemoveObserverCommandFromScope.java
@@ -0,0 +1,28 @@
+package dynamake.commands;
+
+import dynamake.models.CompositeLocation;
+import dynamake.models.Location;
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.transcription.Collector;
+
+public class RemoveObserverCommandFromScope implements Command<Model> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location, ExecutionScope scope) {
+		Location observableLocation = (Location)scope.consume();
+		Location observerLocation = (Location)scope.consume();
+
+		Model observable = (Model)new CompositeLocation(location, observableLocation).getChild(prevalentSystem);
+		Model observer = (Model)new CompositeLocation(location, observerLocation).getChild(prevalentSystem);
+		
+		observable.removeObserver(observer);
+//		System.out.println(observer + " no longer observes " + observable);
+		
+		return null;
+	}
+}
