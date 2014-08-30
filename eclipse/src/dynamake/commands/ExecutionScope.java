7cd12228de2e177a47b23e18416e7ab9122d7d0a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-30 11:41:10 +0200

    Add support for producing/consuming Java nulls in a scope.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/ExecutionScope.java
+++ eclipse/src/dynamake/commands/ExecutionScope.java
@@ -8,15 +8,23 @@ public class ExecutionScope implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private static class NullWrapper {
+		
+	}
 
 	private ArrayDeque<Object> production = new ArrayDeque<Object>();
 	
 	public void produce(Object value) {
+		if(value == null)
+			value = new NullWrapper();
+		
 		production.addLast(value);
 	}
 	
 	public Object consume() {
-		return production.pollFirst();
+		Object value =  production.pollFirst();
+		return value instanceof NullWrapper ? null : value;
 	}
 
 	public void unproduce() {
