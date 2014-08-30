f4d87394ddfc34a8998818be2ae7b5af65b3759b
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-30 11:42:21 +0200

    Added command to set properties by using scope.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/commands/SetPropertyCommandFromScope.java
@@ -0,0 +1,30 @@
+package dynamake.commands;
+
+import dynamake.models.Location;
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.transcription.Collector;
+
+public class SetPropertyCommandFromScope implements Command<Model> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location, ExecutionScope scope) {
+		Model model = (Model)location.getChild(prevalentSystem);
+		
+		String name = (String)scope.consume();
+		Object value = scope.consume();
+		
+		Object previousValue = model.getProperty(name);
+		model.setProperty(name, value, propCtx, 0, collector);
+//		System.out.println("Set property " + name + " to " + value);
+		
+		scope.produce(name);
+		scope.produce(previousValue);
+		
+		return null;
+	}
+}
