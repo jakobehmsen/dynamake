16e9c2743be8803369579378742e1bb68a96a423
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-30 13:26:08 +0200

    Added missing implementation of unwrap command from scope.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/dynamake/commands/UnwrapCommandFromScope.java
+++ eclipse/src/dynamake/commands/UnwrapCommandFromScope.java
@@ -1,17 +1,81 @@
 package dynamake.commands;
 
+import java.util.Hashtable;
+
+import dynamake.models.CanvasModel;
 import dynamake.models.Location;
 import dynamake.models.Model;
 import dynamake.models.PropogationContext;
+import dynamake.numbers.Fraction;
+import dynamake.numbers.RectangleF;
 import dynamake.transcription.Collector;
 
 public class UnwrapCommandFromScope implements Command<Model> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Override
-	public Object executeOn(PropogationContext propCtx, Model prevalentSystem,
-			Collector<Model> collector, Location location, ExecutionScope scope) {
-		// TODO Auto-generated method stub
+	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location, ExecutionScope scope) {
+		Location wrapperLocationInTarget = (Location)scope.consume();
+		RectangleF creationBounds = (RectangleF)scope.consume();
+		@SuppressWarnings("unchecked")
+		Hashtable<Location, Location> wrapperToSourceLocations = (Hashtable<Location, Location>)scope.consume();
+		
+		CanvasModel target = (CanvasModel)location.getChild(prevalentSystem);
+		CanvasModel wrapper = (CanvasModel)wrapperLocationInTarget.getChild(target);
+		
+		Location[] locationsInWrapper = wrapper.getLocations();
+		Model[] models = new Model[wrapper.getModelCount()];
+		for(int i = 0; i <  locationsInWrapper.length; i++) {
+			Model model = wrapper.getModelByLocation(locationsInWrapper[i]);
+			
+			models[i] = model;
+		}
+
+		// Move models from wrapper to target
+		for(int i = 0; i < models.length; i++) {
+			Model model = models[i];
+			wrapper.removeModel(model, propCtx, 0, collector);
+		}
+
+		// Removed wrapper from target
+		target.removeModel(wrapper, propCtx, 0, collector);
+
+		// Offset the coordinates of the moved models
+		for(Model model: models) {
+			Fraction x = (Fraction)model.getProperty("X");
+			Fraction y = (Fraction)model.getProperty("Y");
+			
+			model.setProperty("X", x.add(creationBounds.x), propCtx, 0, collector);
+			model.setProperty("Y", y.add(creationBounds.y), propCtx, 0, collector);
+		}
+		
+		// Move models from wrapper to target
+		for(int i = 0; i < models.length; i++) {
+			Model model = models[i];
+			Location locationInWrapper = locationsInWrapper[i];
+			Location locationInSource = wrapperToSourceLocations.get(locationInWrapper);
+			// If wrapped model was part of a previous wrapping
+			if(locationInSource != null)
+				// then restore id
+				target.restoreModelByLocation(locationInSource, model, propCtx, 0, collector);
+			else
+				// otherwise, it is new and should be given a new location
+				target.addModel(model, propCtx, 0, collector);
+		}
+		
+		Location[] modelLocations = new Location[models.length];
+		for(int i = 0; i < models.length; i++) {
+			Model model = models[i];
+			modelLocations[i] = target.getLocationOf(model);
+		}
+
+		scope.produce(wrapperLocationInTarget);
+		scope.produce(creationBounds);
+		scope.produce(modelLocations);
+		
 		return null;
 	}
-
 }
