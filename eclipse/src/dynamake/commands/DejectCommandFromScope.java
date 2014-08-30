6f56361c629ce87ce5db07fdbf52f3dfec1a0049
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-08-30 12:00:40 +0200

    Added inject- and deject command which uses scope.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/commands/DejectCommandFromScope.java
@@ -0,0 +1,27 @@
+package dynamake.commands;
+
+import dynamake.models.Location;
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.transcription.Collector;
+
+public class DejectCommandFromScope implements Command<Model> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location, ExecutionScope scope) {
+		Location sourceLocation = (Location)scope.consume();
+		Location targetLocation = (Location)scope.consume();
+		
+		Model source = (Model)sourceLocation.getChild(prevalentSystem);
+		Model target = (Model)targetLocation.getChild(prevalentSystem);
+		
+		source.deject(target);
+		
+		// TODO: Consider whether a change should be sent out here
+		return null;
+	}
+}
---
+++ eclipse/src/dynamake/commands/InjectCommandFromScope.java
@@ -0,0 +1,28 @@
+package dynamake.commands;
+
+import dynamake.models.CompositeLocation;
+import dynamake.models.Location;
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.transcription.Collector;
+
+public class InjectCommandFromScope implements Command<Model> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object executeOn(PropogationContext propCtx, Model prevalentSystem, Collector<Model> collector, Location location, ExecutionScope scope) {
+		Location sourceLocation = (Location)scope.consume();
+		Location targetLocation = (Location)scope.consume();
+		
+		Model source = (Model)CompositeLocation.getChild(prevalentSystem, location, sourceLocation);
+		Model target = (Model)CompositeLocation.getChild(prevalentSystem, location, targetLocation);
+		
+		source.inject(target);
+		
+		// TODO: Consider whether a change should be sent out here
+		return null;
+	}
+}
