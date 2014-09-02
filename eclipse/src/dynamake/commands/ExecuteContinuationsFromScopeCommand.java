87aed2aafa5a5dc3d4fdefd70c2baa86f5ec6457
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-02 19:38:34 +0200

    Added ExecuteContinuationsFromScopeCommand to support executing pairs of scope/command, representing continuations, consumed from the scope.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/commands/ExecuteContinuationsFromScopeCommand.java
@@ -0,0 +1,41 @@
+package dynamake.commands;
+
+import java.util.ArrayList;
+
+import dynamake.models.Location;
+import dynamake.models.Model;
+import dynamake.models.PropogationContext;
+import dynamake.models.transcription.ContinueTransactionHandlerFactory;
+import dynamake.transcription.Collector;
+import dynamake.tuples.Tuple2;
+
+public class ExecuteContinuationsFromScopeCommand<T> implements Command<T> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int continuationCount;
+
+	public ExecuteContinuationsFromScopeCommand(int continuationCount) {
+		this.continuationCount = continuationCount;
+	}
+
+	@Override
+	public Object executeOn(PropogationContext propCtx, T prevalentSystem, Collector<T> collector, Location<T> location, ExecutionScope<T> scope) {
+		T reference = location.getChild(prevalentSystem);
+		
+		for(int i = 0; i < continuationCount; i++) {
+			@SuppressWarnings("unchecked")
+			ExecutionScope<T> continuationScope = (ExecutionScope<T>)scope.consume();
+			@SuppressWarnings("unchecked")
+			PURCommand<T> continuationCommand = (PURCommand<T>)scope.consume();
+			
+			collector.startTransaction(reference, new ContinueTransactionHandlerFactory<T>(continuationScope, continuationCommand));
+			// Just that one command is to be executed
+			collector.execute(continuationCommand);
+			collector.commitTransaction();
+		}
+
+		return null;
+	}
+}
