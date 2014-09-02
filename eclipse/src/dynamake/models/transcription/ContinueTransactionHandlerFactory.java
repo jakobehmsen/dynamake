6ad2fbf4fa9ec11b4e7545d0dd713bdaf71227ac
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-02 19:37:40 +0200

    Moved ContinueTransactionHandlerFactory from dynamake.models to dynamake.models.transacription. Made ContinueTransactionHandlerFactory generic.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/dynamake/models/transcription/ContinueTransactionHandlerFactory.java
@@ -0,0 +1,27 @@
+package dynamake.models.transcription;
+
+import dynamake.commands.ExecutionScope;
+import dynamake.commands.PURCommand;
+import dynamake.models.Model;
+import dynamake.transcription.TransactionHandler;
+import dynamake.transcription.TransactionHandlerFactory;
+
+public class ContinueTransactionHandlerFactory<T> implements TransactionHandlerFactory<T> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private ExecutionScope<T> scope;
+	private PURCommand<T> command;
+
+	public ContinueTransactionHandlerFactory(ExecutionScope<T> scope, PURCommand<T> command) {
+		this.scope = scope;
+		this.command = command;
+	}
+
+	@Override
+	public TransactionHandler<T> createTransactionHandler(T reference) {
+		return new ContinueTransactionHandler(scope, command);
+	}
+}
